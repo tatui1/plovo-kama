ec4a5c4f61afd398d6b5df83f06c99cddd750649
tatui1 <kamila.lumazova@compasscollege.art>
2026-03-31 12:41:48 +0600

    Update Header.tsx
    исправляем ошибки

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,33 +2,26 @@ import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link as RouterLink } from 'react-router';
 
-// ВОТ ЗДЕСЬ БЫЛА ОШИБКА: Нужно обязательно добавить totalPrice в интерфейс
 interface Props {
-  totalCount: number;
-  totalPrice: number; // Добавили это поле
+  totalCount: number
+  totalPrice: number
 }
 
 export const Header = ({ totalCount, totalPrice }: Props) => {
   return (
     <AppBar position="static" sx={{ mb: 3 }}>
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        {/* Логотип приложения */}
         <Typography 
           variant="h6" 
           component={RouterLink} 
           to="/" 
-          sx={{ textDecoration: 'none', color: 'inherit', fontWeight: 'bold' }}
-        >
-          Plovo App
+          sx={{ textDecoration: 'none', color: 'inherit', fontWeight: 'bold'}}>Plovo App
         </Typography>
-
-        {/* Кнопка перехода в корзину */}
         <Button 
           component={RouterLink} 
           to="/basket" 
           color="inherit"
-          startIcon={<ShoppingBasketIcon />}
-        >
+          startIcon={<ShoppingBasketIcon />}>
           <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', ml: 1 }}>
             <Typography variant="caption" sx={{ lineHeight: 1 }}>
               Items: <strong>{totalCount}</strong>
@@ -40,5 +33,5 @@ export const Header = ({ totalCount, totalPrice }: Props) => {
         </Button>
       </Toolbar>
     </AppBar>
-  );
-};
+  )
+}
